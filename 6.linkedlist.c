1d54aa750980d1c080cb45830ff2b1ad09eb8903
first reop <shakshisinghj@gmail.com>
2024-04-08 15:27:18 +0530

    wrote a program to create and traverse through a linked list

---
+++ 6.linkedlist.c
@@ -0,0 +1,65 @@
+//to create and traverse in a linked list
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node{
+    int data;
+    struct node *address;
+}node;
+node *head,*new,*temp;
+void create()
+{
+    new=(node*)malloc(sizeof(node));
+    printf("enter data in node");
+    scanf("%d",&new->data);
+    new->address=NULL;
+    if(head==NULL)
+    {
+        head=new;
+        temp=new;
+    }
+    else{
+        temp->address=new;
+        temp=new;
+    }
+    
+
+}
+void display()
+{
+    node *ptr;
+    ptr=head;
+    if(head==NULL)
+    {
+        printf("list is empty");
+    }
+    else
+    {
+        printf("elemnts of the linked list are\n");
+        while(ptr!=NULL)
+        {
+            printf("%d\n",ptr->data);
+            ptr=ptr->address;
+        }
+    }
+}
+void main()
+{   
+    
+    int choice;
+    do{
+    printf("1.create\n2.display\n3.exit\n");
+    printf("enter the choice:");
+    scanf("%d",&choice);
+    {
+    switch(choice)
+    {
+        case 1: create(); break;
+        case 2: display(head); break;
+        case 3: printf("exted"); break;
+        deafult: printf("invalid input. try again");
+    }
+    }
+}
+ while(choice!=3);
+}
