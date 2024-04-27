763531155f563f6ee87fc81a49d2f3e63424beab
first reop <shakshisinghj@gmail.com>
2024-04-27 21:10:51 +0530

    writing comments explaining the algorithm and for better understandability of code

--- 13.countingSort.c
+++ 13.countingSort.c
@@ -5,13 +5,13 @@ int main() {
     int size,count=0;
     printf("enter the size of array");
     scanf("%d",&size);
-    int arr[size],outputArray[size];
+    int arr[size],outputArray[size]; //output array will be used to later to store sorted elements
     printf("enter the elements of array");
     for(int i=0;i<size;i++)
     {
         scanf("%d",&arr[i]);
-    }
-    
+    }  
+    //finding maximum number in the given array
     int max=arr[0];
     for(int i=1;i<size;i++)
     {
@@ -20,12 +20,14 @@ int main() {
             max=arr[i];
         }
     }
+    //initializing all values in countingArray to zero
     int countingArray[max+1];
     for(int i=0;i<size;i++)
     {
         countingArray[i]=0;
     }
     int c=0;
+    //writing the frequency of each element in the given array in thier respecitve index
     for(int j=0;j<=max;j++)
     {
         count=0;
@@ -39,12 +41,13 @@ int main() {
         countingArray[c]=count;
         c++;
     }
-    printf("\n");
+    // doing prefix sum or cummulative sum
     for(int i=1;i<size;i++)
     {
         countingArray[i]=countingArray[i]+countingArray[i-1];
     }
     int x1,x2,x3;
+    //storing value in outputArray
     for(int i=size-1;i>=0;i--)
     {
         x1=arr[i];
@@ -59,3 +62,4 @@ int main() {
     }
 }
 
+// this algorithm has time complexity of O(M+N), where M is size of given array and N is size of countingArray
