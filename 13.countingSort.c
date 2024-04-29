d9f2528616aacb889637eb705d377e304392b543
first reop <shakshisinghj@gmail.com>
2024-04-29 13:22:37 +0530

    fixing a bug

--- 13.countingSort.c
+++ 13.countingSort.c
@@ -1,10 +1,11 @@
 //implementing counting algorithm: counting sort algorithm is used for sorting small and positive integers
 #include <stdio.h>
 int main() {
-    int size,count=0;
+    int size;
     printf("enter the size of array");
     scanf("%d",&size);
-    int arr[size],outputArray[size]; //output array will be used to later to store sorted elements
+    int arr[size],outputArray[size];
+    //output array will be used to later to store sorted elements
     printf("enter the elements of array");
     for(int i=0;i<size;i++)
     {
@@ -21,39 +22,24 @@ int main() {
     }
     //initializing all values in countingArray to zero
     int countingArray[max+1];
-    for(int i=0;i<size;i++)
+    for(int i=0;i<max+1;i++)
     {
         countingArray[i]=0;
     }
-    int c=0;
     //writing the frequency of each element in the given array in thier respecitve index
-    for(int j=0;j<=max;j++)
+    for(int j=0;j<size;j++)
     {
-        count=0;
-        for(int i=0;i<size;i++)
-        {
-           if(arr[i]==c)
-           {
-               count++;
-           }
-        }
-        countingArray[c]=count;
-        c++;
+         countingArray[arr[j]]++;
     }
     // doing prefix sum or cummulative sum
-    for(int i=1;i<size;i++)
+    for(int i=1;i<=size;i++)
     {
         countingArray[i]=countingArray[i]+countingArray[i-1];
     }
-    int x1,x2,x3;
     //storing value in outputArray
     for(int i=size-1;i>=0;i--)
     {
-        x1=arr[i];
-        x2=countingArray[x1];
-        x3=x2-1;
-        outputArray[x3]=x1;
-        countingArray[x1]=countingArray[x1]-1;
+       outputArray[--countingArray[arr[i]]]=arr[i];
     }
     for(int i=0;i<size;i++)
     {
