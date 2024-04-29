a978843ede499d49c8a7c434002d3916b9c585eb
first reop <shakshisinghj@gmail.com>
2024-04-29 16:25:05 +0530

    fixing a bug

--- 13.countingSort.c
+++ 13.countingSort.c
@@ -4,13 +4,16 @@ int main() {
     int size;
     printf("enter the size of array");
     scanf("%d",&size);
+    
     int arr[size],outputArray[size];
     //output array will be used to later to store sorted elements
+    
     printf("enter the elements of array");
     for(int i=0;i<size;i++)
     {
         scanf("%d",&arr[i]);
     }  
+    
     //finding maximum number in the given array
     int max=arr[0];
     for(int i=1;i<size;i++)
@@ -20,27 +23,32 @@ int main() {
             max=arr[i];
         }
     }
+    
     //initializing all values in countingArray to zero
     int countingArray[max+1];
     for(int i=0;i<max+1;i++)
     {
         countingArray[i]=0;
     }
+    
     //writing the frequency of each element in the given array in thier respecitve index
     for(int j=0;j<size;j++)
     {
          countingArray[arr[j]]++;
     }
+    
     // doing prefix sum or cummulative sum
-    for(int i=1;i<=size;i++)
+    for(int i=1;i<=max;i++)
     {
         countingArray[i]=countingArray[i]+countingArray[i-1];
     }
+   
     //storing value in outputArray
     for(int i=size-1;i>=0;i--)
     {
        outputArray[--countingArray[arr[i]]]=arr[i];
     }
+    
     for(int i=0;i<size;i++)
     {
         printf("%d ",outputArray[i]);
