156e6041a0b6dab34d064d3f3ffbb42bc29cb64e
first reop <shakshisinghj@gmail.com>
2024-05-06 08:37:39 +0530

    added options of peek, isEmpty, isFull

--- StackUsingArray.c
+++ StackUsingArray.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 int top=-1;
-#define maxsize 10
+#define maxsize 5
 int arr[maxsize];
 
 void push()
@@ -30,6 +30,28 @@ void pop()
        printf("Popped Element:%d\n",temp);
    }
 }
+void peek()
+{
+    if(top==-1)
+       printf("stack underflow");
+    else{
+        printf("%d",arr[top]);
+    }
+}
+void isEmpty()
+{
+    if(top==-1)
+      printf("True");
+    else 
+      printf("False");
+}
+void isFull()
+{
+    if(top==maxsize-1)
+      printf("True");
+    else 
+      printf("False");
+}
 void display()
 {
     if(top==-1)
@@ -50,15 +72,18 @@ void main()
      printf("****************STACK****************\n");
     int choice;
     do{
-        printf("enter choice:\n1.push\n2.pop\n3.display\n4.Exit\n");
+        printf("\nEnter choice:\n1.push\n2.pop\n3.display\n4.peek\n5.isEmpty\n6.isFull\n7.Exit\n");
         scanf("%d",&choice);
         switch(choice)
         {
             case 1: push(); break;
             case 2: pop(); break;
             case 3: display(); break;
-            case 4: printf("EXITED"); break;
+            case 4: peek(); break;
+            case 5: isEmpty(); break;
+            case 6: isFull(); break;
+            case 7: printf("EXITED"); break;
             default: printf("Invalid input,try again.\n");
         }
-    }while(choice!=4);
+    }while(choice!=7);
 }
